0548409e595b9319b8ce387294caf9a64094d6bc
Cuma Ugur <cumaugur@Cumas-MBP.attlocal.net>
2024-05-16 21:48:36 -0500

    python fundamentals

---
+++ handson python/testpython.py
@@ -0,0 +1,44 @@
+print('Hello, world!')
+
+a=7
+b=5.5
+print(a)
+print(b)
+
+
+score=88
+if score>90:
+    print("Well done! That gets you an A+ grade")
+elif score>=70:
+    print("Good work! Passed the test!")
+else:
+    print("Better luck next time!")
+    
+    
+myarray=[20, 10, 44, 928, 200]
+for x in myarray:
+    print(x)
+    
+    
+    
+    
+shopping_list = ["bread", "water", "chips", "candy", "soda"]
+for x in shopping_list:
+  print(x)
+  if x == "chips":
+    break
+
+
+
+Score = 60
+while Score < 90:
+  print(Score)
+  Score += 5
+  
+  
+A = 1
+while A < 36:
+  print(A)
+  if A > 20:
+    break
+  A += 6
